7b5092335d69da2df46694f809c19a6333844b81
Dave Perryman <dave@bugsnag.com>
2016-09-21 19:06:44 +0100

    Added simple project that depends on Bugsnag

---
+++ app/src/main/java/com/bugsnag/android/bugsnagndk/MainActivity.java
@@ -0,0 +1,28 @@
+package com.bugsnag.android.bugsnagndk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.bugsnag.android.Bugsnag;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bugsnag.init(this);
+        Button clickButton = (Button) findViewById(R.id.notifyButton);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bugsnag.getClient().notify(new RuntimeException("test notify"));
+            }
+        });
+
+
+    }
+}
