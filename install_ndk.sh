cb3a861892f91e992a2a8dc5411a070a4ec786e2
fractalwrench <fractalwrench@gmail.com>
2018-06-15 11:34:26 +0100

    ci: attempt setting ANDROID_NDK to workaround ndk cmake issue

--- install_ndk.sh
+++ install_ndk.sh
@@ -4,15 +4,16 @@ export OS_VERSION='darwin-x86_64'
 export NDK_VERSION='r16b'
 
 echo "Downloading NDK, this will take a while..."
-wget https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-${OS_VERSION}.zip
+wget -q https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-${OS_VERSION}.zip
 
 echo "Unzipping NDK, this will take a while..."
-unzip android-ndk-${NDK_VERSION}-${OS_VERSION}.zip
+unzip -q android-ndk-${NDK_VERSION}-${OS_VERSION}.zip
 
 echo "Copying to ndk-bundle"
 mv android-ndk-${NDK_VERSION} $ANDROID_HOME/ndk-bundle
 
 export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
+export ANDROID_NDK=$ANDROID_HOME/ndk-bundle
 
 # add to PATH
 export PATH=${PATH}:${ANDROID_NDK_HOME}
